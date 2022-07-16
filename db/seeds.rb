645c3db7a607b383044986aaff9addeb03a28293
Marcos Spomberg <marcosspomberg@gmail.com>
2022-07-16 19:24:14 -0400

    Create users array

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 require 'faker'
+require 'http'
 
 puts "Seeding data..."
 
@@ -8,11 +9,16 @@ puts "Creating users..."
 
 User.destroy_all
 
-5.times do |index|
+5.times do
   User.create!(username: Faker::Internet.unique.username, email: Faker::Internet.unique.email, password: Faker::Internet.password)
 end
 
-user = User.first
+# Creates an array with all the users
+users = []
+
+User.each do |user|
+  users.push(user)
+end
 
 # Lists
 
@@ -20,6 +26,15 @@ puts "Creating lists..."
 
 List.destroy_all
 
+def generate_list_array 
+  output = []
 
+  10.times do
+    movie_id = rand(2...99999)
+    output.push(rand(2...99999))
+  end
+
+  output
+end
 
 puts "Done seeding!"
