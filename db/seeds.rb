b69d7138327de179bdec7824b8d069b553767d93
Marcos Spomberg <marcosspomberg@gmail.com>
2022-07-16 17:18:39 -0400

    Create user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,24 @@ require 'faker'
 
 puts "Seeding data..."
 
+# Users
+
+puts "Creating users..."
+
+User.destroy_all
+
+5.times do |index|
+  User.create!(username: Faker::Internet.unique.username, email: Faker::Internet.unique.email, password: Faker::Internet.password)
+end
+
+user = User.first
+
+# Lists
+
+puts "Creating lists..."
+
+List.destroy_all
+
+
+
 puts "Done seeding!"
