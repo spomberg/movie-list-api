87dc958ddddfd61cfa2d1873967aeba51fb8bb9b
Marcos Spomberg <marcosspomberg@gmail.com>
2022-07-16 17:16:52 -0400

    Change name field to username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User
   include Mongoid::Document
-  field :name, type: String
+  field :username, type: String
   field :email, type: String
   field :password, type: String
   has_many :lists
