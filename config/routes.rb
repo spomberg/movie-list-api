a790b5043e55973e88506c2821822a6496f63884
Marcos Spomberg <marcosspomberg@gmail.com>
2022-07-17 00:04:32 -0400

    Set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,19 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root to: 'lists#index'
+
+  resources :lists, only: [:index, :show, :new, :edit, :search, :destroy]
+
+  resources :users, only: [:index]
+
+  get '/lists' => 'lists#index'
+  get '/users' => 'users#index'
+
+  post '/add-list' => 'list#new'
+  post '/edit-list' => 'list#edit'
+  post '/delete-list' => 'list#destroy'
+  post '/movie-search'
+
 end
