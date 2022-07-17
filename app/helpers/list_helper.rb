12744774fbee43de5fc77198214239d03f353da5
Marcos Spomberg <marcosspomberg@gmail.com>
2022-07-16 21:46:08 -0400

    Implement is_movie)id_valid logic

--- app/helpers/list_helper.rb
+++ app/helpers/list_helper.rb
@@ -3,7 +3,14 @@ require 'http'
 module ListHelper
 
   def is_movie_id_valid(id)
+    movie_details = (HTTP.get("https://api.themoviedb.org/3/movie/#{id}?api_key=#{ENV['TMDB_API_KEY']}&language=en-US")).parse
+
+    if movie_details['original_title'] != nil
+      return true
+    end
+    
     false
+  
   end
 
 end
