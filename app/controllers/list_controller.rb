b793e92b999a93e4e9abc7d07fd756874dd2226c
Marcos Spomberg <marcosspomberg@gmail.com>
2022-07-17 09:48:19 -0400

    Order lists by created_on date

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -2,14 +2,15 @@ class ListController < ApplicationController
   def index
     output = []
 
-    lists = List.where('is_public = true')
+    lists = List.where('is_public = true').order_by([:created_on, :desc])
 
     lists.each do |list|
 
       output.push({
         id: list["_id"],
         title: list["title"],
-        username: User.find(list["user_id"])["username"]
+        username: User.find(list["user_id"])["username"],
+        created_on: list["created_on"]
       })
     end
 
