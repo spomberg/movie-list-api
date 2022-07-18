d02a34277bf7e848204fed1cb5be78bfbea35c53
Marcos Spomberg <marcosspomberg@gmail.com>
2022-07-18 19:52:17 -0400

    Implement list#show controller method

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,4 +1,7 @@
 class ListController < ApplicationController
+  require 'http'
+  include ListHelper
+
   def index
     output = []
 
@@ -13,6 +16,26 @@ class ListController < ApplicationController
         created_on: list["created_on"]
       })
     end
+
+    render json: output.to_json
+  end
+
+  def show
+    list = List.find(params[:id])
+    movies = []
+    list["movies"].each do |movie|
+      movies.push(extract_movie_info(movie))
+    end
+
+    output = {
+      id: list["_id"],
+      title: list["title"],
+      username: User.find(list["user_id"])["username"],
+      created_on: list["created_on"],
+      description: list["description"],
+      is_public: list["is_public"],
+      movies: movies
+    }
 
     render json: output.to_json
   end
