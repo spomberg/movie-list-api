84b5c7a1079e96c22d8691bf0d172d3f7ebd5dd7
Marcos Spomberg <marcosspomberg@gmail.com>
2022-07-18 19:50:52 -0400

    Create extract_movie_info helper method

--- app/helpers/list_helper.rb
+++ app/helpers/list_helper.rb
@@ -13,4 +13,16 @@ module ListHelper
   
   end
 
+  def extract_movie_info(id)
+    movie_details = (HTTP.get("https://api.themoviedb.org/3/movie/#{id}?api_key=#{ENV['TMDB_API_KEY']}&language=en-US")).parse
+    
+    return {
+      original_title: movie_details['original_title'],
+      overview: movie_details['overview'],
+      poster_path: "https://image.tmdb.org/t/p/original#{movie_details['poster_path']}",
+      release_date: movie_details['release_date'],
+      runtime: movie_details['runtime']
+    }
+  end
+
 end
