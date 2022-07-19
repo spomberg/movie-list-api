b99f944aabeb9d958c73dd0d9f5f2104fc1993c0
Marcos Spomberg <marcosspomberg@gmail.com>
2022-07-19 18:47:17 -0400

    Implement get_directors helper method

--- app/helpers/list_helper.rb
+++ app/helpers/list_helper.rb
@@ -13,15 +13,30 @@ module ListHelper
   
   end
 
+  def get_directors(movie)
+    output = []
+    
+    movie["credits"]["crew"].each do |crew_member|
+      if crew_member["job"] == "Director"
+        output.push(crew_member["name"])
+      end
+    end
+    
+    output
+  end
+
   def extract_movie_info(id)
-    movie_details = (HTTP.get("https://api.themoviedb.org/3/movie/#{id}?api_key=#{ENV['TMDB_API_KEY']}&language=en-US")).parse
+    movie_details = (HTTP.get("https://api.themoviedb.org/3/movie/#{id}?api_key=#{ENV['TMDB_API_KEY']}&&append_to_response=credits")).parse
     
     return {
+      id: movie_details['id'],
       original_title: movie_details['original_title'],
       overview: movie_details['overview'],
       poster_path: "https://image.tmdb.org/t/p/original#{movie_details['poster_path']}",
       release_date: movie_details['release_date'],
-      runtime: movie_details['runtime']
+      runtime: movie_details['runtime'],
+      directed_by: get_directors(movie_details),
+      cast: movie_details["credits"]["cast"]
     }
   end
 
