480c33c1d20d407e35e58b9e3c572d8d5368fa7b
Marcos Spomberg <marcosspomberg@gmail.com>
2022-07-30 14:06:16 -0400

    Refactor tests

--- spec/helpers/list_helper_spec.rb
+++ spec/helpers/list_helper_spec.rb
@@ -5,30 +5,30 @@ RSpec.configure do |c|
   c.include ListHelper
 end
 
-RSpec.describe List, type: :model do
+RSpec.describe 'is_movie_id_valid' do
   context "with a valid movie id" do
-    xit "returns true" do
+    it "returns true" do
       output = is_movie_id_valid(500)
       expect(output).to eq true
     end
   end
 
   context "with an invalid movie id" do
-    xit "returns false" do
+    it "returns false" do
       output = is_movie_id_valid(95518)
       expect(output).to eq false
     end
   end
 
   context "with a valid adult movie id" do
-    xit "returns false" do
+    it "returns false" do
       output = is_movie_id_valid(177637)
       expect(output).to eq false
     end
   end
 
   context "with a valid index and array" do
-    xit "moves the element up" do
+    it "moves the element up" do
       mock_arr = [1, 2, 3, 4, 5]
       output = move_up(mock_arr, 2)
       expected_result = [1, 3, 2, 4, 5]
@@ -38,11 +38,53 @@ RSpec.describe List, type: :model do
   end
 
   context "if the index is 0" do
-    xit "doesn't do anything" do
+    it "doesn't do anything" do
       mock_arr = [1, 2, 3, 4, 5]
       output = move_up(mock_arr, 0)
       expected_result = mock_arr
 
+      expect(output).to eq expected_result
+    end
+  end
+  context "with a valid index and array" do
+    it "moves the element up" do
+      mock_arr = [1, 2, 3, 4, 5]
+      output = move_up(mock_arr, 2)
+      expected_result = [1, 3, 2, 4, 5]
+
+      expect(output).to eq expected_result
+    end
+  end
+
+  context "if the index is 0" do
+    it "doesn't do anything" do
+      mock_arr = [1, 2, 3, 4, 5]
+      output = move_up(mock_arr, 0)
+      expected_result = mock_arr
+
+      expect(output).to eq expected_result
+    end
+  end
+  
+end
+
+RSpec.describe 'move_up' do
+  context "with a valid index and array" do
+    it "moves the element up" do
+      mock_arr = [1, 2, 3, 4, 5]
+      output = move_up(mock_arr, 2)
+      expected_result = [1, 3, 2, 4, 5]
+  
+      expect(output).to eq expected_result
+    end
+  end
+  
+  context "if the index is 0" do
+    it "doesn't do anything" do
+      mock_arr = [1, 2, 3, 4, 5]
+      output = move_up(mock_arr, 0)
+      expected_result = mock_arr
+  
       expect(output).to eq expected_result
     end
   end
