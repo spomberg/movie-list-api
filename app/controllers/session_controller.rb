14797a6a91280b7d59a20e8e81bf2a17f617556f
Marcos Spomberg <marcosspomberg@gmail.com>
2022-08-03 17:33:42 -0400

    Generate session controller

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,36 @@
+class SessionController < ApplicationController
+  require 'nanoid'
+  
+  def signup
+    user = User.new(_id: Nanoid.generate(size: 8), username:params[:username], email: params[:email], password: params[:password])
+
+    # if user is saved
+    if user.save
+      # we encrypt user info using the pre-define methods in application controller
+      token = encode_user_data({ user_data: user.id })
+
+      # return to user
+      render json: { token: token }
+    else
+      # render error message
+      render json: { message: "invalid credentials" }
+    end
+  end
+
+  def login
+    user = User.find_by(email: params[:email])
+
+    # you can use bcrypt to password authentication
+    if user && user.password == params[:password]
+
+      # we encrypt user info using the pre-define methods in application controller
+      token = encode_user_data({ user_data: user.id })
+
+      # return to user
+      render json: { token: token }
+    else
+      render json: { message: "invalid credentials" }
+    end
+  end
+
+end
