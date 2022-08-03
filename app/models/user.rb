77d7165e83fced61d620cfcf41f977fb3264684b
Marcos Spomberg <marcosspomberg@gmail.com>
2022-08-03 18:27:06 -0400

    Add uniqueness validator to email and username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User
   include Mongoid::Document
+
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :username, presence: true
+
+  validates_uniqueness_of :email, :username
+
   field :_id, type: String
   field :username, type: String
   field :email, type: String
