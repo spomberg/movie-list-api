b2085c2a51456f3ed34f8306a07570da7d335b57
Marcos Spomberg <marcosspomberg@gmail.com>
2022-08-03 19:12:03 -0400

    Import get_user_id helper method to find the user_id

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,21 +1,29 @@
 class UserController < ApplicationController
-  include ListHelper
+  include ListHelper, UserHelper
 
   def show
-    lists = List.where(:user_id => "0").order_by([:created_on, :desc])
+    if authentication
 
-    output = []
-    
-    lists.each do |list|
-      output.push({
-        id: list["_id"],
-        title: list["title"],
-        created_on: list["created_on"]
-      })
+      user_id = get_user_id
+
+      lists = List.where(:user_id => user_id).order_by([:created_on, :desc])
+  
+      output = []
+      
+      lists.each do |list|
+        output.push({
+          id: list["_id"],
+          title: list["title"],
+          created_on: list["created_on"]
+        })
+      end
+      
+      output
+  
+      render json: { code: 200, list: output }
+    else
+      render json: { message: "Invalid credentials!" }
     end
-    
-    output
 
-    render json: output.to_json
   end
 end
