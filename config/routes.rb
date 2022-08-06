1759598d91e2d1586e34d17b0da3c9d4acc70b00
Marcos Spomberg <marcosspomberg@gmail.com>
2022-08-06 00:02:37 -0400

    Create logout route

--- config/routes.rb
+++ config/routes.rb
@@ -11,16 +11,17 @@ Rails.application.routes.draw do
   get 'api/lists/' => 'list#index'
   get 'api/lists/:id' => 'list#show'
   get 'api/lists/:id/edit-view' => 'list#edit_view'
-
+  
   get 'api/user/' => 'user#authenticate'
   get 'api/user/lists' => 'user#view_lists'
-
+  
   post 'api/list/new' => 'list#new'
   post 'api/login' => 'session#login'
   post 'api/signup' => 'session#signup'
-
+  
   put 'api/list/:id/edit' => 'list#edit'
   put 'api/list/search-movie' => 'list#search'
+  put 'api/logout' => 'session#logout'
   
   delete 'api/list/:id/delete' => 'list#destroy'
   
