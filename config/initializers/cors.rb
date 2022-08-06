b49ff2a37954676c5a48eb91271f7e8c9ad2ceb2
Marcos Spomberg <88166154+spomberg@users.noreply.github.com>
2022-08-06 15:46:23 -0400

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins ENV['ORIGIN']
-    resource '*', headers: :any, methods: [:get, :post, :put, :delete], credentials: true
+    resource '*', headers: :any, methods: [:get, :post, :put, :delete]
   end
 end
