10ff95d028806d52ca64eb589bc27d69733c10b6
Marcos Spomberg <marcosspomberg@gmail.com>
2022-08-07 23:04:23 -0400

    Remove root-to config

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  root to: 'list#index'
-
   resources :list, :user, :session
 
   get 'api/lists/' => 'list#index'
