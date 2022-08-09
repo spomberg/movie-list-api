f99ea4f422ebe5e7cbfd5b06e6ced5c1dfd8af39
Marcos Spomberg <marcosspomberg@gmail.com>
2022-08-09 00:24:57 -0400

    Set cookie to nil upon logouy

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -17,7 +17,7 @@ class SessionController < ApplicationController
       # if user is saved
       if user.save
         # return to user
-        cookies.encrypted[:user_id] = { value: user['_id'], expires: 7.days, same_site: 'Strict', secure: true }
+        cookies.encrypted[:user_id] = { value: user['_id'], expires: 7.days, same_site: 'Strict' }
       else
         # render error message
         render json: { message: "Invalid credentials" }
@@ -30,7 +30,7 @@ class SessionController < ApplicationController
       user = User.find_by(:email => params[:email])
   
       if user && BCrypt::Password.new(user["password_hash"]) == params[:password]
-        cookies.encrypted[:user_id] = { value: user['_id'], expires: 7.days, same_site: 'Strict', secure: true }
+        cookies.encrypted[:user_id] = { value: user['_id'], expires: 7.days, same_site: 'Strict' }
       else
         render json: { message: "Invalid credentials!" }
       end
@@ -40,7 +40,7 @@ class SessionController < ApplicationController
   end
 
   def logout
-    cookies.delete :user_id
+    cookies[:user_id] = nil
   end
 
 end
